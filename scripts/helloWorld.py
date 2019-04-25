005f861f4dbc143c010c628c2ee4b15adaa84984
Krystian Franus <krystian.franus@gmail.com>
2019-04-25 18:21:13 +0200

    add Dockerfile, requirements and modify helloWorld script so that it runs in the background

--- scripts/helloWorld.py
+++ scripts/helloWorld.py
@@ -1,1 +1,9 @@
-print("Hello, World!")
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+sched = BlockingScheduler()
+
+@sched.scheduled_job('interval', seconds=2)
+def hello_world_job():
+    print("Hello, World!")
+
+sched.start()
