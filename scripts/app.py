50eb09a20810f1792c8c20f5fded51195b7e97b7
emeraldPrism <piotr.golab@gmail.com>
2019-06-04 22:07:02 +0200

    rework of diff system to find diffs between iterations

--- scripts/app.py
+++ scripts/app.py
@@ -14,6 +14,8 @@ DEFAULT_INTERVAL = 10
 
 interval, activity_intervals, monitors = operations.check_command_line_args()
 
+global_temp = {}
+
 sched = BlockingScheduler()
 @sched.scheduled_job('interval', seconds = interval)
 def automatic_client_job():
@@ -22,13 +24,19 @@ def automatic_client_job():
 	then for each metric it builds ranking of hosts based on their load using given metrics
 	and compares the list of active hosts to previous one, then logs the results to the console.
 	"""
+	
+	global global_temp
+	
 	for monitor_index, monitor_addr in enumerate(monitors):
 		logger.log_monitor_header(monitor_addr)
 		host_list, host_names_set = operations.retrieve_all_hosts_per_monitor(monitor_addr)
 		metrics = operations.retrieve_all_existing_metrics_for_monitor(monitor_addr)
 		for metric in metrics:
-			filename = "temp/temp_" + str(monitor_index) + "_" + metric + ".txt"
-			prev_hosts = file_operations.read_hosts_from_temp(filename, "r")
+		
+			var_name = "temp_" + str(monitor_index) + "_" + metric
+			if var_name not in global_temp.keys():
+				global_temp[var_name] = []			
+			prev_hosts = global_temp[var_name]
 		
 			rank, active_hosts = operations.build_rank(monitor_addr, host_list, metric, activity_intervals)
 			logger.log_rank(rank, metric)
@@ -38,7 +46,8 @@ def automatic_client_job():
 			add_strs = compare_operations.find_added_hosts(prev_hosts, host_names_set)
 			logger.log_multiple_messages(rem_strs)
 			logger.log_multiple_messages(add_strs)
-			file_operations.write_hosts_to_temp(host_names_set, filename,"w+")
+			
+			global_temp[var_name] = list(host_names_set)
 			logger.log_separator(1)
 			
 		logger.log_separator(1)
